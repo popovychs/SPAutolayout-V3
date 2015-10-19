465d17d6f5ce3fdf87bc5fe58142bebbce47835f
Stanislav Popovych <popovychs@gmail.com>
2015-10-19 20:33:02 +0300

    Initial Commit

---
+++ SPAutolayout-V3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPAutolayout-V3
+//
+//  Created by popovychs on 19.10.15.
+//  Copyright © 2015 popovychs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SPAutolayout-V3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SPAutolayout-V3
+//
+//  Created by popovychs on 19.10.15.
+//  Copyright © 2015 popovychs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
