e370ea5f032956bb2293e29dd64c5c4691e1559a
Stanislav Popovych <popovychs@gmail.com>
2015-10-19 21:58:46 +0300

    it works

--- SPAutolayout-V3/SPControllers/ViewController.m
+++ SPAutolayout-V3/SPControllers/ViewController.m
@@ -14,20 +14,52 @@
 @property (weak, nonatomic) IBOutlet UIButton *clearButton;
 @property (weak, nonatomic) IBOutlet UIButton *loadButton;
 
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view removeConstraints:self.view.constraints];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[showLabel]-25-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:@{@"showLabel":_showLabel}]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[showLabel]->=100-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:@{@"showLabel":_showLabel}]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[clearButton(60)]->=0-[loadButton(60)]-10-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:@{@"clearButton":_clearButton, @"loadButton":_loadButton}]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|->=0-[clearButton(30)]-10-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:@{@"clearButton":_clearButton}]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|->=0-[loadButton(30)]-10-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:@{@"loadButton":_loadButton}]];
 }
 
 - (IBAction)loadText:(UIButton *)sender {
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Data" ofType:@"txt"];
+    
+    NSString *textFromFile = [NSString stringWithContentsOfFile:filePath
+                                                       encoding:NSMacOSRomanStringEncoding
+                                                          error:nil];
+    _showLabel.text = textFromFile;
 }
 
 - (IBAction)clearButton:(UIButton *)sender {
+    _showLabel.text = @"";
 }
 
 @end
